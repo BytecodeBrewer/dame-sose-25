0e4d5814ea22dec7d3bd6ca55759be3cf545c60c
orwasaker2electricboogaloo <sakerorwa96@gmail.com>
2025-08-17 22:46:02 +0200

    Implemented ErrMes, added undo move, and promote to Dame, and expanded Player

--- model/Board.java
+++ model/Board.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Board {
+    public enum ErrMes {
+        NONE,
+        INVALID_MOVE,
+        FIELD_OCCUPIED,
+        OUT_OF_BOUNDS
+        // Add more error types as needed
+    }
+
     private Piece[][] board;
     private final int SIZE = 8;
     private Player player1; // Spieler für weiße Steine
@@ -10,9 +19,13 @@ public class Board {
     private List<Runnable> changeListeners = new ArrayList<>();
     private boolean[][] selectedSquares;
 
+    // Error handling 
+    private ErrMes errmes = ErrMes.NONE;
+    private Stack<Piece[][]> history = new Stack<>();
+
     public Board(Player player1, Player player2) {
-        this.player1 = player1; // Weiß
-        this.player2 = player2; // Schwarz
+        this.player1 = player1;
+        this.player2 = player2;
         board = new Piece[SIZE][SIZE];
         selectedSquares = new boolean[SIZE][SIZE];
         initializeBoard();
@@ -24,9 +37,13 @@ public class Board {
                 selectedSquares[i][j] = false;
                 if ((i + j) % 2 != 0) {
                     if (i < 3) {
-                        occupyField(i, j, new Piece("black", "man", player2));
+                        Piece p = new Piece(Piece.PieceColor.BLACK, Piece.PieceType.MAN, player2, i, j);
+                        occupyField(i, j, p);
+                        player2.addPiece(p);
                     } else if (i > 4) {
-                        occupyField(i, j, new Piece("white", "man", player1));
+                        Piece p = new Piece(Piece.PieceColor.WHITE, Piece.PieceType.MAN, player1, i, j);
+                        occupyField(i, j, p);
+                        player1.addPiece(p);
                     } else {
                         freeField(i, j);
                     }
@@ -38,21 +55,17 @@ public class Board {
     }
 
     public void initialize() {
-        // Neues Spiel initialisieren
         initializeBoard();
     }
 
-    // Liefert zurück, ob das Feld frei ist
     public boolean isFieldFree(int x, int y) {
         return board[x][y] == null;
     }
 
-    // Markiert das Feld als besetzt
     public void occupyField(int x, int y, Piece piece) {
         board[x][y] = piece;
     }
 
-    // Markiert das Feld als frei
     public void freeField(int x, int y) {
         board[x][y] = null;
     }
@@ -60,13 +73,73 @@ public class Board {
     public Piece getPieceAt(int x, int y) {
         return board[x][y];
     }
-    
+
     public void setPieceAt(int x, int y, Piece piece) {
         board[x][y] = piece;
     }
 
-    public Piece[][] getBoard() {
-        return board;
+    // Error handling
+    public ErrMes getErrmes() {
+        return errmes;
+    }
+
+    // Move logic with error handling and undo
+    public boolean tryMove(int fromX, int fromY, int toX, int toY) {
+        saveState();
+        if (outOfBounds(fromX, fromY) || outOfBounds(toX, toY)) {
+            errmes = ErrMes.OUT_OF_BOUNDS;
+            return false;
+        }
+        if (!isFieldFree(toX, toY)) {
+            errmes = ErrMes.FIELD_OCCUPIED;
+            return false;
+        }
+        Piece piece = getPieceAt(fromX, fromY);
+        if (piece == null) {
+            errmes = ErrMes.INVALID_MOVE;
+            return false;
+        }
+        setPieceAt(toX, toY, piece);
+        freeField(fromX, fromY);
+        piece.move(toX, toY);
+        errmes = ErrMes.NONE;
+        // Promotion logic
+        if ((piece.getColor() == Piece.PieceColor.WHITE && toX == 0) ||
+                (piece.getColor() == Piece.PieceColor.BLACK && toX == SIZE - 1)) {
+            promotetoDame(toX, toY);
+        }
+        return true;
+    }
+
+    private boolean outOfBounds(int x, int y) {
+        return x < 0 || x >= SIZE || y < 0 || y >= SIZE;
+    }
+
+    public void promotetoDame(int x, int y) {
+        Piece piece = getPieceAt(x, y);
+        if (piece != null && piece.getType() == Piece.PieceType.MAN) {
+            piece.setType(Piece.PieceType.DAME);
+        }
+    }
+
+    // Undo functionality
+    private void saveState() {
+        Piece[][] copy = new Piece[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                Piece p = board[i][j];
+                if (p != null) {
+                    copy[i][j] = p.copy();
+                }
+            }
+        }
+        history.push(copy);
+    }
+
+    public void undoMove() {
+        if (!history.isEmpty()) {
+            board = history.pop();
+        }
     }
 
     // Fügt einen Listener für Änderungen hinzu
--- model/Piece.java
+++ model/Piece.java
@@ -1,22 +1,45 @@
 public class Piece {
-    private String color;
-    private String type;
+    public enum PieceType {
+        MAN, DAME
+    }
+
+    public enum PieceColor {
+        WHITE, BLACK
+    }
+
+    private PieceColor color;
+    private PieceType type;
     private boolean isCaptured;
     private Player owner;
+    private int x, y; // Position on board
 
-    public Piece(String color, String type, Player owner) {
+    public Piece(PieceColor color, PieceType type, Player owner, int x, int y) {
         this.color = color;
         this.type = type;
         this.isCaptured = false;
         this.owner = owner;
+        this.x = x;
+        this.y = y;
     }
 
-    public String getColor() {
+    // Copy constructor for undo functionality
+    public Piece copy() {
+        Piece copy = new Piece(this.color, this.type, this.owner, this.x, this.y);
+        if (this.isCaptured)
+            copy.capture();
+        return copy;
+    }
+
+    public PieceColor getColor() {
         return color;
     }
 
-    public String getType() {
+    public PieceType getType() {
         return type;
+    }
+
+    public void setType(PieceType type) {
+        this.type = type;
     }
 
     public boolean isCaptured() {
@@ -27,16 +50,34 @@ public class Piece {
         this.isCaptured = true;
     }
 
-    public void move(int newX, int newY) {
-        // Spiel-Logik für das Bewegen des Stücks
-    }
-
-    public boolean canMove(int targetX, int targetY) {
-        // Logik, um zu überprüfen, ob das Stück zu der angegebenen Position erlaubt ist
-        return true;
+    public void uncapture() {
+        this.isCaptured = false;
     }
 
     public Player getOwner() {
         return owner;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void move(int newX, int newY) {
+        this.x = newX;
+        this.y = newY;
+    }
+
+    public boolean canMove(int targetX, int targetY) {
+        // Implement checkers movement rules here
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + type + " at (" + x + "," + y + ")";
     }
 }
