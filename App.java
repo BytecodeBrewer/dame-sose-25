c9ccf307c903fc8fcf542e4edd2a91dec6883d56
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2025-09-07 12:57:02 +0200

    Fixed a startframe bug

--- App.java
+++ App.java
@@ -7,7 +7,7 @@ public class App {
         GameController gameController = new GameController();
         javax.swing.SwingUtilities.invokeLater(() -> {
 
-            new MainView(null);
+            //new MainView(null);
             new StartFrame();  // Hier wird der Controller übergeben
         });
         
